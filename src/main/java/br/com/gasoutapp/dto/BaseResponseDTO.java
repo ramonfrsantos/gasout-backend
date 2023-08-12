72e4ea004f5e61e1d73ca6b435d11a5abbf4030b
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2023-08-12 01:06:43 -0300

    fix retorno user

--- src/main/java/br/com/gasoutapp/dto/BaseResponseDTO.java
+++ src/main/java/br/com/gasoutapp/dto/BaseResponseDTO.java
@@ -1,12 +1,14 @@
 package br.com.gasoutapp.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BaseResponseDTO {
 
--- src/main/java/br/com/gasoutapp/dto/UserDTO.java
+++ src/main/java/br/com/gasoutapp/dto/UserDTO.java
@@ -4,11 +4,13 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import br.com.gasoutapp.domain.User;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDTO {
 	@Size(min = 2, message = "O nome deve conter no minimo 2 caracteres.")
 	private String name;
