ab0f9cbaa9942eda370d02db37fda5790844c659
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2023-08-15 18:11:33 -0300

    teste db url

--- src/main/java/br/com/gasoutapp/Application.java
+++ src/main/java/br/com/gasoutapp/Application.java
@@ -1,5 +1,6 @@
 package br.com.gasoutapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,8 +15,13 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 public class Application {
 
     // http://localhost:8888/swagger-ui/index.html
+	
+	@Value("${spring.datasource.url}")
+	private static String url;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        
+        System.out.println(url);
     }
 }
