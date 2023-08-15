7e08fbf8553be73b714bd0f241ac66498d36b8f9
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2023-08-15 18:38:55 -0300

    update

--- src/main/java/br/com/gasoutapp/Application.java
+++ src/main/java/br/com/gasoutapp/Application.java
@@ -1,6 +1,5 @@
 package br.com.gasoutapp;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,14 +13,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 @SecurityScheme(name = "gasoutapp", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class Application {
 
-    // http://localhost:8888/swagger-ui/index.html
-	
-	@Value("${spring.datasource.url}")
-	private static String url;
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        
-        System.out.println(url);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
