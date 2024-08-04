1b014026060410e466214a9ba217e3d020bf2b89
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2024-08-04 11:06:45 -0300

    Fix decrypt and encrypt method toget secret key from a file

--- src/main/java/br/com/gasoutapp/infrastructure/config/security/EncryptorCustom.java
+++ src/main/java/br/com/gasoutapp/infrastructure/config/security/EncryptorCustom.java
@@ -8,9 +8,13 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
-import java.io.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.util.Base64;
 
 /**
@@ -21,74 +25,15 @@ import java.util.Base64;
 public class EncryptorCustom {
     private static final String ALGORITHM = "AES/GCM/NoPadding";
     private static final int GCM_TAG_LENGTH = 16;
-    private static final int GCM_NONCE_LENGTH = 12;
-    private static final String KEY_FILE_PATH = "secretKey.key";
-    private static final String NONCE_FILE_PATH = "nonce.key";
+    private static final byte[] nonce = new byte[12];
+    private static final String KEY_FILE = "secret.key";
 
-    private static SecretKey secretKey;
-    private static byte[] nonce;
+    private static final SecretKey secretKey;
 
     static {
-        File keyFile = new File(KEY_FILE_PATH);
-        if (keyFile.exists()) {
-            getSecretFromFile(keyFile);
-        } else {
-            generateNewKeySecretFile(keyFile);
-        }
-
-        File nonceFile = new File(NONCE_FILE_PATH);
-        if (nonceFile.exists()) {
-            getNonceFromFile(nonceFile);
-        } else {
-            generateNewNonceFile(nonceFile);
-        }
+        Path path = Paths.get(KEY_FILE);
+        secretKey = getSecretFromPath(path);
     }
-
-    private static void generateNewKeySecretFile(File keyFile) {
-        try (FileOutputStream fos = new FileOutputStream(keyFile)) {
-            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-            keyGen.init(256);
-            secretKey = keyGen.generateKey();
-
-            fos.write(secretKey.getEncoded());
-        } catch (IOException | NoSuchAlgorithmException e) {
-            throw new EncryptionException(e.getMessage());
-        }
-    }
-
-    static void getSecretFromFile(File keyFile) {
-        try (FileInputStream fis = new FileInputStream(keyFile)) {
-            byte[] keyBytes = new byte[(int) keyFile.length()];
-            int bytesRead = fis.read(keyBytes);
-            if(bytesRead > 0)
-                secretKey = new javax.crypto.spec.SecretKeySpec(keyBytes, "AES");
-        } catch (IOException e) {
-            throw new EncryptionException(e.getMessage());
-        }
-    }
-
-    private static void generateNewNonceFile(File nonceFile) {
-        try (FileOutputStream fos = new FileOutputStream(nonceFile)) {
-            nonce = new byte[GCM_NONCE_LENGTH];
-            new SecureRandom().nextBytes(nonce);
-
-            fos.write(nonce);
-        } catch (IOException e) {
-            throw new EncryptionException(e.getMessage());
-        }
-    }
-
-    static void getNonceFromFile(File nonceFile) {
-        try (FileInputStream fis = new FileInputStream(nonceFile)) {
-            nonce = new byte[(int) nonceFile.length()];
-            int bytesRead = fis.read(nonce);
-            if(bytesRead > 0)
-                nonce = new byte[GCM_NONCE_LENGTH];
-        } catch (IOException e) {
-            throw new EncryptionException(e.getMessage());
-        }
-    }
-
 
     public static String encrypt(String plaintext) {
         try {
@@ -98,7 +43,7 @@ public class EncryptorCustom {
 
             byte[] ciphertext = cipher.doFinal(plaintext.getBytes());
             return Base64.getEncoder().encodeToString(ciphertext);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new EncryptionException(e.getMessage());
         }
     }
@@ -112,9 +57,26 @@ public class EncryptorCustom {
             byte[] decodedCiphertext = Base64.getDecoder().decode(ciphertext);
             byte[] plaintext = cipher.doFinal(decodedCiphertext);
             return new String(plaintext);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new EncryptionException(e.getMessage());
         }
     }
 
+    private static SecretKey getSecretFromPath(Path path) {
+        try {
+            if (!Files.exists(path)) {
+                KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+                keyGen.init(256);
+                var newKey = keyGen.generateKey();
+                Files.write(path, newKey.getEncoded(), StandardOpenOption.CREATE);
+
+                return newKey;
+            }
+
+            byte[] keyBytes = Files.readAllBytes(path);
+            return new SecretKeySpec(keyBytes, "AES");
+        } catch (NoSuchAlgorithmException | IOException e) {
+            throw new EncryptionException(e.getMessage());
+        }
+    }
 }
